87644630f07f61e3bd960aade08b221fc0192a4e
Muhammad Talha <talha0750@gmail.com>
2023-09-24 17:41:58 +0500

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Muhammad Talha",
+  title: "MERN STACK DEVELOPER & SOCIAL MEDIA MANAGER",
+  email: "muhammad.talha12@studentambassadors.com",
+  gitHub: "MTalhaofc",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/muhammad-talha-offofc ",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "MuhammadTalha",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/dev3.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "developer with books and laptop";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Linkedin",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "My linkedin Profile",
+    url: "https://www.linkedin.com/in/muhammad-talha-offofc ",
   },
   {
-    title: "Web Development for Beginners",
+    title: "My Behance Profile",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "All Behance Profile",
+    url: "https://www.behance.net/muhammadtalhaoff",
   },
   {
     title: "My Resume Site",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Made by Drag and Drop in sites",
+    url: "https://sites.google.com/view/talhadev ",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "MLSA Video",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Video interview for mlsa",
+    url: "https://youtu.be/CYYCW3ii8j4?si=FQWKWmHVOhcBvees",
   },
 ];
 
